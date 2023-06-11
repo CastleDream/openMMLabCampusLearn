c135eddfcfc5a329393c422cfe051b45cd81a912
CastleDream <hs8023hfp@gmail.com>
2023-06-11 22:00:04 +0800

    update exercise_3

---
+++ Exercise_3/rtmdet-tiny_1xb12-40e_balloon.py
@@ -0,0 +1,82 @@
+
+_base_='/content/mmdetection/configs/rtmdet/rtmdet-ins_tiny_8xb32-300e_coco.py'
+
+train_batch_size_per_gpu=4
+train_num_workers=2
+
+val_batch_size_per_gpu=1
+val_num_workers=1
+
+num_classes=1
+
+model =dict(
+  backbone = dict(frozen_stages=4),
+  bbox_head=dict(dict(num_classes=num_classes))
+)
+
+base_lr = train_batch_size_per_gpu*0.04/(32*8)
+
+max_epochs=40
+num_epochs_stage2=5
+
+load_from = 'https://download.openmmlab.com/mmdetection/v3.0/rtmdet/rtmdet-ins_tiny_8xb32-300e_coco/rtmdet-ins_tiny_8xb32-300e_coco_20221130_151727-ec670f7e.pth'
+data_root = '/content/balloon_dataset/balloon/'
+
+metainfo={
+  'classes':('balloon',),
+  'palette':[(220,20,60),]
+}
+
+train_dataloader = dict(
+    batch_size=train_batch_size_per_gpu,
+    num_workers=train_num_workers,
+    pin_memory=True,
+    dataset=dict(
+        data_root=data_root,
+        metainfo=metainfo,
+        data_prefix=dict(img='train/'),
+        ann_file='train_coco.json'
+    )
+)
+
+val_dataloader = dict(
+    batch_size=val_batch_size_per_gpu,
+    num_workers=val_num_workers,
+    dataset=dict(
+        data_root=data_root,
+        metainfo=metainfo,
+        data_prefix=dict(img='val/'),
+        ann_file='val_coco.json'
+   )
+)
+test_dataloader = val_dataloader
+
+param_scheduler = [
+    dict(
+        type='LinearLR',
+        start_factor=1.0e-5,
+        by_epoch=False,
+        begin=0,
+        end=30),
+    dict(
+        # use cosine lr from 10 to 20 epoch
+        type='CosineAnnealingLR',
+        eta_min=base_lr * 0.05,
+        begin=max_epochs // 2, 
+        end=max_epochs,
+        T_max=max_epochs // 2,
+        by_epoch=True,
+        convert_to_iter_based=True),
+]
+optim_wrapper = dict(optimizer=dict(lr=base_lr))
+
+_base_.custom_hooks[1].switch_epoch = max_epochs-num_epochs_stage2
+
+val_evaluator = dict(ann_file=data_root + 'val_coco.json')
+test_evaluator=val_evaluator
+
+default_hooks = dict(
+    checkpoint=dict(interval=10,max_keep_ckpts=2, save_best='auto'),
+    logger=dict(type='LoggerHook', interval=5))
+train_cfg= dict(max_epochs=max_epochs, val_interval=10)
+
